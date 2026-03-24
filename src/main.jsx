259807e7d02a5ddb9c0489680bf99fee874283c3
sunrise8030 <sunrise8030@gmail.com>
2026-03-24 12:21:24 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-// ============ main.jsx
+// ============ src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./styles.css";
+// import "./styles.css"; // ✅ kaldır
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
